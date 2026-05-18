694ae2d60aab7f2cf2788f7eca2cc8f0e3cf5e02
ConstructoDestructo <ric.herrera.654@gmail.com>
2026-05-17 20:03:57 -0700

    Update hack_tokenizer.c
    Tokenizer should now theoretically work, but it still needs to be tested

--- HACK-MIPS-ASSEMBLER/src/hack_tokenizer.c
+++ HACK-MIPS-ASSEMBLER/src/hack_tokenizer.c
@@ -1,6 +1,42 @@
 #include "D:/Github_local_repos/Personal_Projects/HACK-MIPS-ASSEMBLER/include/hack_tokenizer.h"
 
 
+Token tokenize(char *line)
+{
+
+  Token returnToken = {.type = IGNORE, .value = ""};
+
+  switch(*line)
+  {
+    case '@':
+    {
+      returnToken.type = A_INSTRUCTION;
+      copyToValueMember(returnToken.value, line,returnToken.type);
+      break;
+    }
+    case '(':
+    {
+      returnToken.type = LABEL;
+      copyToValueMember(returnToken.value, line,returnToken.type);
+      break;
+    }
+
+    case '/':
+    break;
+    case '\0':
+    break;
+
+    default:
+    {
+      returnToken.type = C_INSTRUCTION;
+      copyToValueMember(returnToken.value, line,returnToken.type);
+      break;
+    }
+  }
+
+  return returnToken;
+}
+
 static char *trim(char *line)
 {
      char *placeHolder = line;
@@ -23,3 +59,46 @@ static char *trim(char *line)
 
      return placeHolder;
 }
+
+static void copyToValueMember(char *value, char *line, TokenType type)
+{
+  int i = 0;
+      
+  while(*line != '\0')
+  {
+
+    if(i < 99)
+    {
+      if(type == A_INSTRUCTION)
+      {
+        line ++;
+        value[i] = *line;
+        i++;
+      }
+      else if (type == LABEL && *line != ')')
+      {
+        //(LOOP)
+        line ++;
+        if(*line != ')')
+        {
+          value[i] = *line;
+          i++;
+        }
+      }
+      else if(type == C_INSTRUCTION)
+      {
+        value[i] = *line;
+        line ++;
+          i++;
+      }
+      else
+      {
+        line++;
+      }
+    }
+  }
+  value[i] = '\0';
+  
+}
+
+
